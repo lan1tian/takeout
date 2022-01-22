ecb507d1825b07ad65e24403d6f01c67ee4c0830
lan1tian <1361959119@qq.com>
2022-01-22 22:08:19 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+const appData = require('./data.json')
+const seller = appData.seller
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          './src/theme'
+        ]
+      }
+    }
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+    }
+  }
+}
